15906e89a2c0079050b6480a687ab7bdab7ac4c2
Greg Bruening <gregbruening@fastmail.com>
2025-12-21 12:22:51 -0500

    Track generated token files in repo
    Resolves #7

---
+++ dist/react-native/variables.js
@@ -0,0 +1,179 @@
+/**
+ * Do not edit directly, this file was auto-generated.
+ */
+
+export const jfColorBlue100 = "#cff3ff";
+export const jfColorBlue200 = "#8ee2ff";
+export const jfColorBlue300 = "#46cfff";
+export const jfColorBlue400 = "#00b9fa";
+export const jfColorBlue500 = "#00a4dd";
+export const jfColorBlue600 = "#0094cc";
+export const jfColorBlue700 = "#0092d0";
+export const jfColorBlue800 = "#004f6a";
+export const jfColorBlue900 = "#00455d";
+export const jfColorGray100 = "#f0f0f0";
+export const jfColorGray200 = "#d2d2d2";
+export const jfColorGray300 = "#a1a1a1";
+export const jfColorGray400 = "#888888";
+export const jfColorGray500 = "#606060";
+export const jfColorGray600 = "#454545";
+export const jfColorGray700 = "#3b3b3b";
+export const jfColorGray800 = "#303030";
+export const jfColorGray900 = "#252525";
+export const jfColorRed100 = "#ffcccc";
+export const jfColorRed200 = "#ffaaac";
+export const jfColorRed300 = "#ff7e80";
+export const jfColorRed400 = "#ff676a";
+export const jfColorRed500 = "#f43c3f";
+export const jfColorRed600 = "#f42023";
+export const jfColorRed700 = "#c80003";
+export const jfColorRed800 = "#b30003";
+export const jfColorRed900 = "#820002";
+export const jfColorOrange100 = "#ffdfcc";
+export const jfColorOrange200 = "#ffccad";
+export const jfColorOrange300 = "#ffb88c";
+export const jfColorOrange400 = "#ff9a5c";
+export const jfColorOrange500 = "#f27223";
+export const jfColorOrange600 = "#e35a05";
+export const jfColorOrange700 = "#ca4e00";
+export const jfColorOrange800 = "#b24400";
+export const jfColorOrange900 = "#a33f00";
+export const jfColorLime100 = "#f0ffda";
+export const jfColorLime200 = "#d6ffc4";
+export const jfColorLime300 = "#caff80";
+export const jfColorLime400 = "#b8ff55";
+export const jfColorLime500 = "#94d737";
+export const jfColorLime600 = "#69b400";
+export const jfColorLime700 = "#559200";
+export const jfColorLime800 = "#4a8000";
+export const jfColorLime900 = "#417000";
+export const jfColorNeutralLight = "#ffffff";
+export const jfColorNeutralDark = "#151515";
+export const jfRadiusNone = { original: "0", number: 0, decimal: 0, scale: 0 };
+export const jfRadiusDefault = {
+  original: "8px",
+  number: 8,
+  decimal: 0.08,
+  scale: 128,
+};
+export const jfRadiusCircle = {
+  original: "999px",
+  number: 999,
+  decimal: 9.99,
+  scale: 15984,
+};
+export const jfSpace2xs = {
+  original: "0.125rem",
+  number: 0.125,
+  decimal: 0.00125,
+  scale: 2,
+};
+export const jfSpaceXs = {
+  original: "0.25rem",
+  number: 0.25,
+  decimal: 0.0025,
+  scale: 4,
+};
+export const jfSpaceSm = {
+  original: "0.5rem",
+  number: 0.5,
+  decimal: 0.005,
+  scale: 8,
+};
+export const jfSpaceMd = {
+  original: "1rem",
+  number: 1,
+  decimal: 0.01,
+  scale: 16,
+};
+export const jfSpaceLg = {
+  original: "1.5rem",
+  number: 1.5,
+  decimal: 0.015,
+  scale: 24,
+};
+export const jfSpaceXl = {
+  original: "2rem",
+  number: 2,
+  decimal: 0.02,
+  scale: 32,
+};
+export const jfSpace2xl = {
+  original: "2.5rem",
+  number: 2.5,
+  decimal: 0.025,
+  scale: 40,
+};
+export const jfSpace3xl = {
+  original: "3rem",
+  number: 3,
+  decimal: 0.03,
+  scale: 48,
+};
+export const jfTypographyFontFamilyFigtree = [
+  "Figtree",
+  "Verdana",
+  "sans-serif",
+];
+export const jfTypographyFontFamilyNotoSans = [
+  "'Noto Sans'",
+  "Arial",
+  "sans-serif",
+];
+export const jfTypographyFontSize2xs = {
+  original: "0.625rem",
+  number: 0.625,
+  decimal: 0.00625,
+  scale: 10,
+};
+export const jfTypographyFontSizeXs = {
+  original: "0.75rem",
+  number: 0.75,
+  decimal: 0.0075,
+  scale: 12,
+};
+export const jfTypographyFontSizeSm = {
+  original: "0.875rem",
+  number: 0.875,
+  decimal: 0.00875,
+  scale: 14,
+};
+export const jfTypographyFontSizeMd = {
+  original: "1rem",
+  number: 1,
+  decimal: 0.01,
+  scale: 16,
+};
+export const jfTypographyFontSizeLg = {
+  original: "1.125rem",
+  number: 1.125,
+  decimal: 0.01125,
+  scale: 18,
+};
+export const jfTypographyFontSizeXl = {
+  original: "1.25rem",
+  number: 1.25,
+  decimal: 0.0125,
+  scale: 20,
+};
+export const jfTypographyFontSize2xl = {
+  original: "1.5rem",
+  number: 1.5,
+  decimal: 0.015,
+  scale: 24,
+};
+export const jfTypographyFontSize3xl = {
+  original: "2rem",
+  number: 2,
+  decimal: 0.02,
+  scale: 32,
+};
+export const jfTypographyWeight100 = 100;
+export const jfTypographyWeight200 = 200;
+export const jfTypographyWeight300 = 300;
+export const jfTypographyWeight400 = 400;
+export const jfTypographyWeight500 = 500;
+export const jfTypographyWeight600 = 600;
+export const jfTypographyWeight700 = 700;
+export const jfTypographyWeight800 = 800;
+export const jfTypographyWeight900 = 900;
